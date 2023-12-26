0e5f91f3f6558e9aace7b9108f3f5923b65f4c8e
Yakubu Adeyemi Oseni <yao@lho.dk>
2023-12-26 11:54:50 +0100

    First Commit

---
+++ InventoryTracking/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventoryTracking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
